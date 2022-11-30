2c8b30ba98feaa02d4211597b31c46aaab588cd2
afrain <calixtoafrain@gmail.com>
2022-11-29 23:01:47 -0300

    Criando a classe abstract Conteudo

--- src/main/java/br/edu/domain/Main.java
+++ src/main/java/br/edu/domain/Main.java
@@ -1,12 +1,30 @@
 package br.edu.domain;
 
+import java.time.LocalDate;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		Curso curso1 = new Curso();
+		curso1.setTitulo("Java");
+		curso1.setDescricao("Descrição curso Java");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso1.setTitulo("JavaScript");
+		curso1.setDescricao("Descrição curso JavaScript");
+		curso1.setCargaHoraria(4);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo("Mentoria de Java");
+		mentoria.setDescricao("Descrição mentoria Java");
+		mentoria.setData(LocalDate.now());
+		
 		
 		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria);
 
 	}
 
